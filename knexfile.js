20752abd5f8fd6457b09036341acf644ea46867b
mcneilr2 <robyn55555@gmail.com>
2024-11-26 15:03:46 -0800

    basic routing and data seeding with two example spots to sql backend, routed to /spots

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,5 @@
 // Import dotenv to process environment variables from `.env` file.
 import "dotenv/config";
-
 export default {
   client: "mysql2",
   connection: {
@@ -8,6 +7,6 @@ export default {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    charset: "utf8",
+    charset: "utf8"
   },
 };
