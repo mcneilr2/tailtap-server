6e0a711578fc9fc18ed98245438d7c5da757b91f
mcneilr2 <robyn55555@gmail.com>
2024-11-27 12:57:21 -0800

    completed get calls for /, /spots, /spots/id

--- controllers/spotsController.js
+++ controllers/spotsController.js
@@ -10,3 +10,21 @@ export const index = async (_req, res) => {
     res.status(400).send(`Error retrieving Inventories: ${err}`);
   }
 };
+
+export const getSpotbyId = async (req, res) => {
+ const { id } = req.params;
+    try {
+      const spot = await knex("spots")
+        .where({ id: Number(id) })
+        .first();
+  
+      if (!spot) {
+        return res.status(404).json({ message: "Spot not found" });
+      }
+      res.status(200).json(spot);
+    } catch (error) {
+      res.status(500).json({
+        message: "Unable to find spot",
+      });
+    }
+  };
--- routes/spotsRoute.js
+++ routes/spotsRoute.js
@@ -3,6 +3,12 @@ import * as spotsController from "../controllers/spotsController.js";
 
 const router = express.Router();
 
-router.route("/").get(spotsController.index);
+router
+    .route("/")
+    .get(spotsController.index);
+
+router
+    .route("/:id")
+    .get(spotsController.getSpotbyId);
 
 export default router;
